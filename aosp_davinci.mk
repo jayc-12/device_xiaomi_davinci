99688a1f80f1408fb9404b5c455a144ed4de6d09
jaychauhan <jaychauhan121999@gmail.com>
2022-04-04 17:10:17 +0000

    arcanify

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
@@ -24,4 +24,21 @@ PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-SELINUX_IGNORE_NEVERALLOWS=true
+#SELINUX_IGNORE_NEVERALLOWS=true
+
+# maintainer flag
+ARCANA_MAINTAINER := Jayc-12
+
+# pixel build flag, if not define build type is vanilla
+WITH_GAPPS := true
+
+# disable/enable blur support, default is false
+TARGET_SUPPORTS_BLUR := true
+
+# prebuilt graphene camera flag, default is false
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# whether to copy apns-conf.xml to system
+# (for devices having issues with TelephonyProvider: FileNotFoundException)
+# default is false (product)
+COPY_APN_SYSTEM := true
